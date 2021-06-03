64496883f25de2f0d05b98ac7840d90bf3b10e93
Julian Zammit <zammit.julian@gmail.com>
2021-06-03 22:12:34 +0100

    Additional API Test

--- tests/api.test.js
+++ tests/api.test.js
@@ -1,11 +1,16 @@
-const request = require('supertest')
-const app = require('../server')
+const request = require("supertest");
+const app = require("../server");
 
-describe('GET board with invalid characters and length', () => {
-  it('should return a 400 error', async () => {
-    const board = 'abc1234'
-    const res = await request(app)
-    .get(`/?board=${board}`)
-    .expect(400)
-  })
-})
+describe("GET board with valid characters and length", () => {
+  it("should return a 200 status", () => {
+    const board = "   x     ";
+    request(app).get(`?board=${board}`).expect(200);
+  });
+});
+
+describe("GET board with invalid characters and length", () => {
+  it("should return a 400 error", async () => {
+    const board = "abc1234";
+    const res = await request(app).get(`?board=${board}`).expect(400);
+  });
+});
