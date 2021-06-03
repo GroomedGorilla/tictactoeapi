39f553a7736145e44435f31ebe0aae3971c2650f
Julian Zammit <juilan@homeppl.com>
2021-06-03 20:56:58 +0100

    Check if game is over

--- board.js
+++ board.js
@@ -19,16 +19,16 @@ const playerCharacter = "x";
 const serverCharacter = "o";
 
 const isValidBoard = (board) => {
-  // board length
+  // board length limited to 9
   const validBoardLength = board.length == 9;
-  // allowed characters
+  // allowed characters (x,o ' ')
   const usesAllowedCharacters = /^[x\o\ ]+$/.test(board);
   // at least one empty space (catches case where game is already over)
   const emptySpot = board.includes(" ");
 
   const numberOfPlayerTurns = (board.match(/x/g) || []).length;
   const numberOfServerTurns = (board.match(/o/g) || []).length;
-  // const gameAlreadyOver = gameOver(board);
+  const gameAlreadyOver = gameOver(board);
 
   // Server can play if:
   // - the board is empty.
@@ -45,7 +45,7 @@ const isValidBoard = (board) => {
     usesAllowedCharacters,
     emptySpot,
     serversTurn,
-    //   !gameAlreadyOver,
+    !gameAlreadyOver,
   ];
 
   //Returns true if board passes all checks
@@ -53,11 +53,11 @@ const isValidBoard = (board) => {
 };
 
 const playerWins = (board, player = playerCharacter) => {
-    // find indices for player moves
-    const playerMoveIndexes = board
+  // find indices for player moves
+  const playerMoveIndexes = board
     .split("")
     .flatMap((char, i) => (char == player ? i : []));
-    // check if player moves satisfy any of the winning combos
+  // check if player moves satisfy any of the winning combos
   const playerWins = winningCombos.map((combo) =>
     combo.reduce(
       (acc, current) => acc & playerMoveIndexes.includes(current),
@@ -67,7 +67,27 @@ const playerWins = (board, player = playerCharacter) => {
   return playerWins.includes(1);
 };
 
+const gameOver = (board) => {
+  // check if either player wins
+  if (playerWins(board)) {
+    return playerCharacter;
+  }
+
+  if (playerWins(board, serverCharacter)) {
+    return serverCharacter;
+  }
+
+  // if board has no empty slows and neither player has won,
+  // then it's a tie
+  if (!board.includes(" ")) {
+    return "tie";
+  }
+
+  return false;
+};
+
 module.exports = {
   isValidBoard,
-  playerWins
+  playerWins,
+  gameOver,
 };
--- tests/board.test.js
+++ tests/board.test.js
@@ -1,52 +1,81 @@
-const { isValidBoard, playerWins } = require("../board")
+const { isValidBoard, playerWins, gameOver } = require("../board");
 
 // testing isValidBoard
 describe("Check board validity with short input", () => {
-    it("should return false", () => {
-      const board = "x oxo ";
-      expect(isValidBoard(board)).toBe(false);
-    });
+  it("should return false", () => {
+    const board = "x oxo ";
+    expect(isValidBoard(board)).toBe(false);
   });
+});
 
-  describe("Check board validity where player has played an extra turn", () => {
-    it("should return false", () => {
-      const board = "xxoxox  ";
-      expect(isValidBoard(board)).toBe(false);
-    });
+describe("Check board validity where player has played an extra turn", () => {
+  it("should return false", () => {
+    const board = "xxoxox  ";
+    expect(isValidBoard(board)).toBe(false);
   });
+});
 
-  describe("Check board validity with incorrect character", () => {
-    it("should return false", () => {
-      const board = "x oxp    ";
-      expect(isValidBoard(board)).toBe(false);
-    });
+describe("Check board validity with incorrect character", () => {
+  it("should return false", () => {
+    const board = "x oxp    ";
+    expect(isValidBoard(board)).toBe(false);
   });
+});
 
-  describe("Check board validity when board is full", () => {
-    it("should return false", () => {
-      const board = "ooxxoooxx";
-      expect(isValidBoard(board)).toBe(false);
-    });
+describe("Check board validity when board is full", () => {
+  it("should return false", () => {
+    const board = "ooxxoooxx";
+    expect(isValidBoard(board)).toBe(false);
   });
+});
 
-  describe("Check board validity when game is already over", () => {
-    it("should return false", () => {
-      const board = "xxx o o  ";
-      expect(isValidBoard(board)).toBe(false);
-    });
+describe("Check board validity when game is already over", () => {
+  it("should return false", () => {
+    const board = "xxx o o  ";
+    expect(isValidBoard(board)).toBe(false);
   });
+});
 
 // testing playerWins
 describe("Check board with winning move by player", () => {
-    it("should return true", () => {
-      const board = "x oxo x  ";
-      expect(playerWins(board, "x")).toBe(true);
-    });
+  it("should return true", () => {
+    const board = "x oxo x  ";
+    expect(playerWins(board, "x")).toBe(true);
   });
+});
 
-  describe("Check board when player hasn't won", () => {
-    it("should return true", () => {
-      const board = "x o o x  ";
-      expect(playerWins(board, "x")).toBe(false);
-    });
+describe("Check board when player hasn't won", () => {
+  it("should return true", () => {
+    const board = "x o o x  ";
+    expect(playerWins(board, "x")).toBe(false);
   });
+});
+
+// testing gameOver
+describe("Check if game is over - player wins", () => {
+  it("should return x", () => {
+    const board = "x oxo x  ";
+    expect(gameOver(board)).toBe("x");
+  });
+});
+
+describe("Check if game is over - server wins", () => {
+  it("should return o", () => {
+    const board = "xox oxoo ";
+    expect(gameOver(board)).toBe("o");
+  });
+});
+
+describe("Check if game is over - tie", () => {
+  it("should return tie", () => {
+    const board = "xoxooxoxo";
+    expect(gameOver(board)).toBe("tie");
+  });
+});
+
+describe("Check if game is over - not over", () => {
+  it("should return false", () => {
+    const board = "xox oxo  ";
+    expect(gameOver(board)).toBe(false);
+  });
+});
